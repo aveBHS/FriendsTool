1e988f64a9e79d92366bdfd1cab7bd6673c9e256
User Unknown <aveBHS.ru@gmail.com>
2020-02-16 00:27:05 +0300

    Create install file

---
+++ antidog.sh
@@ -0,0 +1,8 @@
+su
+apt install git
+apt install python3
+apt install python3-pip
+pip3 install pillow
+pip3 install vk_api
+git clone https://github.com/aveBHS/FriendsTool/
+python3 ./FriendsTool/antidog.py
