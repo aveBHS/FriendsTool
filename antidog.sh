3cb10fe8cfbcdef89c1f6efc085b97b7acada99f
User Unknown <aveBHS.ru@gmail.com>
2020-02-16 13:39:59 +0300

    Update

--- antidog.sh
+++ antidog.sh
@@ -1,9 +1,8 @@
-su
-apt install git
-apt install python3
-apt install python3-pip
-pip3 install pillow
-pip3 install vk_api
+sudo apt install git
+sudo apt install python3
+sudo apt install python3-pip
+pip3 install pillow --user
+pip3 install vk_api --user
 git clone https://github.com/aveBHS/FriendsTool/
 clear
 python3 ./FriendsTool/antidog.py
